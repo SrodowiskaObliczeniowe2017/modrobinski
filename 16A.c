3ca46bb77e5cf186e3a53c4cdea81045d89f9909
Live session user <marszalkix@gmail.com>
2017-10-18 21:54:24 +0000

    Solution for 16A Task

---
+++ 16A.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int main() 
+{
+
+  int buff = 0;
+  int i,j,row,col,prew=0;
+  int flaga = 1;
+  char name[100];
+  
+  printf ("File name: ");
+  scanf ("%s", name); 
+
+  FILE* file = fopen (name, "r");
+
+  fscanf (file, "%d", &buff);    
+  row = buff;  
+
+  fscanf (file, "%d", &buff); 
+  col = buff;
+
+  int Tab [col][row]; 
+
+  for (i=0;i<row;i=i+1)
+  {
+	for (j=0;j<col;j=j+1)
+	{
+		fscanf (file, "%1d", &buff);
+		Tab[j][i] = buff;
+	}
+  }
+
+  fclose (file); 
+
+  for (i=0;i<row;i=i+1)
+  {
+	for (j=0;j<col;j=j+1)
+	{
+		if(j==0)
+		{
+		  prew = Tab[j][i];
+		}
+		if(Tab[j][i] != prew)
+		{
+		  flaga = 0;
+		  printf("NO\n");
+		  break;
+		}
+		else
+		{
+		  prew = Tab[j][i];
+		}
+	}
+	
+	if (i != 0 || i == row-1)
+	{
+	   if (prew == Tab[0][i+1])
+	   {
+	      flaga = 0;
+	      printf("NO\n");
+	      break;
+	   }
+	}	  
+  }
+
+  if(flaga == 1)
+  {
+	printf("YES\n");
+  }
+
+
+  return 0;
+}
