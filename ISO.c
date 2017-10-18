b4ac24cb304b11319773b518fe56e1f71b9178c3
student <student@lab404-07.lab.metal.agh.edu.pl>
2017-10-18 16:07:02 +0200

    First atempt

--- ISO.c
+++ ISO.c
@@ -2,9 +2,30 @@
 
 int main() 
 {
+   FILE *fp;
+   char buff[255];
+   int i;
 
+   fp = fopen("Dobry", "r");
+   fscanf(fp, "%s", buff);
+   printf("1 : %s\n", buff );
 
-
+   fgets(buff, 255, (FILE*)fp);
+   printf("2: %s\n", buff );
    
+   fgets(buff, 255, (FILE*)fp);
+   printf("3: %s\n", buff );
+
+   for( i=0; i<rows; i=i+1)
+   {
+	for ( j=0;j<col;j=j+1)
+	{
+
+	}
+   }
+
+   fclose(fp);
+
+
    return 0;
 }
