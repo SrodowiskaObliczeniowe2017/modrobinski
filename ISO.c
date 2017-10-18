92f2fcccbf6f9bd270656316916cfe3becaa898d
student <student@lab404-07.lab.metal.agh.edu.pl>
2017-10-18 16:39:26 +0200

    End

--- ISO.c
+++ ISO.c
@@ -5,36 +5,25 @@ int main()
    int buff = 0;
    int i,j,rows,col;
    int prew = 0;
+   
 
   FILE* file = fopen ("Dobry", "r");
+
   fscanf (file, "%d", &buff);    
-  printf ("%d ", buff);
   rows = buff;  
 
   fscanf (file, "%d", &buff); 
-  printf ("%d ", buff);
   col = buff;
+
+  int Tab [col][rows];
+
+
 
   fscanf (file, "%d", &buff); 
   printf ("%d ", buff);
+  
            
 
-   for( i=0; i<rows; i=i+1)
-   {
-	for ( j=0;j<col;j=j+1)
-	{
-	   fscanf (file, "%d", &buff);
-	   if (j=0)
-	   {
-		
-	   }
-	   else
-	   {
-
-	   }
-	   fscanf (file, "%d", &buff);
-	}
-   }
 
    fclose (file);  
 
